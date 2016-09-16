39c17ff309491818343469f5bc172d046899ae5b
Mirla Braga <mirlabraga@gmail.com>
2016-09-16 17:36:00 -0300

    new screem for testes

---
+++ app/src/main/java/com/example/mirla/adapter/NavigationDrawerAdapter.java
@@ -0,0 +1,64 @@
+package com.example.mirla.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.mirla.activity.R;
+import com.example.mirla.model.NavDrawerItem;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by mirla on 16/09/16.
+ */
+public class NavigationDrawerAdapter extends RecyclerView.Adapter<NavigationDrawerAdapter.MyViewHolder> {
+
+    List<NavDrawerItem> data = Collections.emptyList();
+    private LayoutInflater inflater;
+    private Context context;
+
+
+    public NavigationDrawerAdapter(Context context, List<NavDrawerItem> data) {
+        this.context = context;
+        inflater = LayoutInflater.from(context);
+        this.data = data;
+    }
+
+    public void delete(int position) {
+        data.remove(position);
+        notifyItemRemoved(position);
+    }
+
+
+    @Override
+    public NavigationDrawerAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.nav_drawer_row, parent, false);
+        MyViewHolder holder = new MyViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(NavigationDrawerAdapter.MyViewHolder holder, int position) {
+        View view = inflater.inflate(R.layout.nav_drawer_row, parent, false);
+        new MyViewHolder(view);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView title;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.title);
+        }
+    }
+}
